75f28e0e452aa4d5738a4d9950f7a65bf358f262
ugesh <ugesh.roopchand@hotmail.com>
2018-10-11 11:25:49 +0400

    test

---
+++ exercise.js
@@ -0,0 +1,20 @@
+// if(jQuery){
+//     alert("jQuery Loaded!");
+// }else {
+//     alert("Mo jquery");
+// }
+
+//Select all divs  and give them a purple background
+$("div").css("background","purple");
+
+//Select the divs with class "highlight and make them 200px wide"
+$(".highlight").css("width","200px");
+
+//Select the div with id "third" and give it an organge boder
+$("#third").css("border", "1px solid orange");
+
+
+//Select the first div only and change its font color to pink
+//pusodo select first of  type
+
+$("div:first-of-type").css("background","green");
